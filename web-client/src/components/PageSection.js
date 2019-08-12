c43fef1962782ac85a1a3c90cfad927793ae6155
Dave van Herten <dvanherten@outlook.com>
2019-08-11 22:44:40 -0400

    delete old client

--- web-client/src/components/PageSection.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const PageSection = ({ children }) => {
-  return (
-    <section className="section">
-      <div className="container">{children}</div>
-    </section>
-  );
-};
--- web-client/src/icons.js
+++
@@ -1,6 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
-
-export const registerIcons = () => {
-  library.add(faTrash);
-};
